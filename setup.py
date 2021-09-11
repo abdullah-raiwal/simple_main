e0dd13a53af9a02a886f417ae9c94bb741bb2a49
Abdullah <76896214+AbdullahQaiser858@users.noreply.github.com>
2021-09-11 13:57:00 +0500

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages, version
+
+setup(name = "src",
+     version= "0.0.1",
+     description = "its a mlops project",
+     author = "abdullah",
+     packages = find_packages(),    
+     license = "MIT"
+     )
+     
+
+
